73ae625480788060342fb6f5cff0bf894eff63ca
jiajic <72078254+jiajic@users.noreply.github.com>
2023-09-06 12:22:01 -0400

    Update test-GiottoInstructions.R

--- tests/testthat/test-GiottoInstructions.R
+++ tests/testthat/test-GiottoInstructions.R
@@ -8,19 +8,22 @@ options('giotto.use_conda' = FALSE)
 # silence deprecated internals
 rlang::local_options(lifecycle_verbosity = "quiet")
 
-instrs = createGiottoInstructions(
-  show_plot = TRUE,
-  return_plot = NULL,
-  save_plot = FALSE,
-  save_dir = NULL,
-  plot_format = "png",
-  dpi = 300,
-  units = NULL,
-  height = NULL,
-  width = NULL,
-  is_docker = FALSE,
-  plot_count = 0,
-  fiji_path = NULL)
+suppressWarnings({
+  instrs = createGiottoInstructions(
+    show_plot = TRUE,
+    return_plot = NULL,
+    save_plot = FALSE,
+    save_dir = NULL,
+    plot_format = "png",
+    dpi = 300,
+    units = NULL,
+    height = NULL,
+    width = NULL,
+    is_docker = FALSE,
+    plot_count = 0,
+    fiji_path = NULL)
+})
+
 
 expression_matrix = matrix(1:100, nrow = 10)
 colnames(expression_matrix) = paste0("cell",1:10)
@@ -37,7 +40,7 @@ test_that("Instructions are created", {
 
 # readGiottoInstructions
 test_that("readGiottoInstructions reads a few giotto object params correctly", {
-  
+
   expect_type(readGiottoInstructions(gobject, param = "show_plot"), "logical")
   expect_type(readGiottoInstructions(gobject, param = "plot_format"), "character")
   expect_type(readGiottoInstructions(gobject, param = "dpi"), "double")
