eef5283bec4fdbabd4a32489802a6fdb494f94aa
George Chen <72078254+jiajic@users.noreply.github.com>
2024-05-24 09:21:31 -0400

    change: comment out optional pkgs message

--- R/create.R
+++ R/create.R
@@ -152,30 +152,30 @@ createGiottoObject <- function(expression,
     ## data.table vars
     cell_ID <- feat_ID <- NULL
 
-    ## check if all optional packages are installed
-    # TODO: update at the end
-    # TODO: extract from suggest field of DESCRIPTION
-    extra_packages <- c(
-        "scran", "MAST", "png", "tiff", "biomaRt",
-        "trendsceek", "multinet", "RTriangle", "FactoMineR"
-    )
-
-    pack_index <- extra_packages %in% rownames(utils::installed.packages())
-    extra_installed_packages <- extra_packages[pack_index]
-    extra_not_installed_packages <- extra_packages[!pack_index]
-
-    if (any(pack_index == FALSE) == TRUE) {
-        wrap_msg(
-            "Consider to install these (optional) packages to run all possible",
-            "Giotto commands for spatial analyses: ",
-            extra_not_installed_packages
-        )
-        wrap_msg(
-            "Giotto does not automatically install all these packages as they",
-            "are not absolutely required and this reduces the number of
-            dependencies"
-        )
-    }
+    # ## check if all optional packages are installed
+    # # TODO: update at the end
+    # # TODO: extract from suggest field of DESCRIPTION
+    # extra_packages <- c(
+    #     "scran", "MAST", "png", "tiff", "biomaRt",
+    #     "trendsceek", "multinet", "RTriangle", "FactoMineR"
+    # )
+    # 
+    # pack_index <- extra_packages %in% rownames(utils::installed.packages())
+    # extra_installed_packages <- extra_packages[pack_index]
+    # extra_not_installed_packages <- extra_packages[!pack_index]
+    # 
+    # if (any(pack_index == FALSE) == TRUE) {
+    #     wrap_msg(
+    #         "Consider to install these (optional) packages to run all possible",
+    #         "Giotto commands for spatial analyses: ",
+    #         extra_not_installed_packages
+    #     )
+    #     wrap_msg(
+    #         "Giotto does not automatically install all these packages as they",
+    #         "are not absolutely required and this reduces the number of
+    #         dependencies"
+    #     )
+    # }
 
 
     ## if cores is not set, then set number of cores automatically, but with
