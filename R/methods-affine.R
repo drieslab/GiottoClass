4c00c9fe2999786b7268795f146cdfb3f0ffb5ff
George Chen <72078254+jiajic@users.noreply.github.com>
2024-06-04 14:31:12 -0400

    feat: add inverting of affine transforms

--- R/methods-affine.R
+++ R/methods-affine.R
@@ -9,13 +9,15 @@
 #' @param x object
 #' @param m `matrix` or coercible to `matrix`. Should be a matrix with either
 #' 2 or 3 columns (linear or affine).
+#' @param inv logical. Whether the inverse of the affine transform should
+#' be applied.
 #' @param ... additional args to pass (none implemented)
 #' @returns affine transformed object
 #' @examples
 #' m <- diag(rep(1, 3))
 #' trans_m <- matrix(c(1, 0, 0, 0, 1, 0, 200, 300, 1), nrow = 3)
 #' scale_m <- matrix(c(2, 0, 0, 0, 3, 0, 0, 0, 1), nrow = 3)
-#' aff_m <- matrix(c(2, 3, 0.43, 0.2, 3, 0, 100, 29, 1), nrow = 3)
+#' aff_m <- matrix(c(2, 3, 0, 0.2, 3, 0, 100, 29, 1), nrow = 3)
 #' 
 #' gpoints <- GiottoData::loadSubObjectMini("giottoPoints")
 #' gpoly <- GiottoData::loadSubObjectMini("giottoPolygon")
@@ -25,34 +27,40 @@
 #' plot(affine(gpoints, m))
 #' plot(affine(gpoints, trans_m))
 #' plot(affine(gpoints, scale_m))
-#' plot(affine(gpoints, aff_m))
+#' aff_gpoints <- affine(gpoints, aff_m)
+#' plot(aff_gpoints)
+#' plot(affine(aff_gpoints, aff_m, inv = TRUE))
 #' 
 #' # giottoPolygon #############################################
 #' plot(affine(gpoly, m))
 #' plot(affine(gpoly, trans_m))
 #' plot(affine(gpoly, scale_m))
-#' plot(affine(gpoly, aff_m))
+#' aff_gpoly <- affine(gpoly, aff_m)
+#' plot(aff_gpoly)
+#' plot(affine(aff_gpoly, aff_m, inv = TRUE))
 #'
 #' # spatLocsObj ###############################################
 #' plot(affine(sl, m))
 #' plot(affine(sl, trans_m))
 #' plot(affine(sl, scale_m))
-#' plot(affine(sl, aff_m))
+#' aff_sl <- affine(sl, aff_m)
+#' plot(aff_sl)
+#' plot(affine(aff_sl, aff_m, inv = TRUE))
 NULL
 # ---------------------------------------------------------------- #
 
 #' @rdname affine
 #' @export
-setMethod("affine", signature("SpatVector"), function(x, m, ...) {
-    .affine_sv(x, m, ...)
+setMethod("affine", signature("SpatVector"), function(x, m, inv = FALSE, ...) {
+    .affine_sv(x, m, inv, ...)
 })
 
 #' @rdname affine
 #' @export
 setMethod(
     "affine", signature("giottoPoints"),
-    function(x, m, ...) {
-        x[] <- .affine_sv(x = x[], m = m, ...)
+    function(x, m, inv = FALSE, ...) {
+        x[] <- .affine_sv(x = x[], m = m, inv = inv, ...)
         return(x)
     }
 )
@@ -61,8 +69,8 @@ setMethod(
 #' @export
 setMethod(
     "affine", signature("giottoPolygon"),
-    function(x, m, ...) {
-        .do_gpoly(x, what = .affine_sv, args = list(m = m, ...))
+    function(x, m, inv = FALSE, ...) {
+        .do_gpoly(x, what = .affine_sv, args = list(m = m, inv = inv, ...))
     }
 )
 
@@ -70,8 +78,10 @@ setMethod(
 #' @export
 setMethod(
     "affine", signature("spatLocsObj"),
-    function(x, m, ...) {
-        x[] <- .affine_dt(x = x[], m = m, xcol = "sdimx", ycol = "sdimy", ...)
+    function(x, m, inv = FALSE, ...) {
+        x[] <- .affine_dt(
+            x = x[], m = m, xcol = "sdimx", ycol = "sdimy", inv = inv, ...
+        )
         return(x)
     }
 )
@@ -82,38 +92,52 @@ setMethod(
 # internals ####
 
 # 2D only
-.affine_sv <- function(x, m, ...) {
+.affine_sv <- function(x, m, inv = FALSE, ...) {
     m <- as.matrix(m)
     gtype <- terra::geomtype(x)
     xdt <- data.table::as.data.table(x, geom = "XY")
     
     xdt <- .affine_dt(
-        x = xdt, m = m, xcol = "x", ycol = "y", ...
+        x = xdt, m = m, xcol = "x", ycol = "y", inv = inv, ...
     )
     
     res <- switch(gtype,
-                  "points" = terra::vect(xdt, geom = c("x", "y")),
-                  "polygons" = terra::as.polygons(xdt)
+      "points" = terra::vect(xdt, geom = c("x", "y")),
+      "polygons" = terra::as.polygons(xdt)
     )
     
     return(res)
 }
 
-.affine_dt <- function(x, m, xcol = "sdimx", ycol = "sdimy", ...) {
+.affine_dt <- function(
+        x, m, xcol = "sdimx", ycol = "sdimy", inv = FALSE, ...
+) {
     x <- data.table::as.data.table(x)
     m <- as.matrix(m)
     xm <- as.matrix(x[, c(xcol, ycol), with = FALSE])
     
-    # linear transforms
-    aff_m <- m[seq(2), seq(2)]
-    xm <- xm %*% aff_m
-    
     # translations (if any)
+    trans <- NULL
     if (ncol(m) > 2) {
         translation <- m[seq(2), 3] # class: numeric
+        if (isTRUE(inv)) translation <- -translation
+    }
+    
+    # inv translation
+    if (!is.null(trans) && isTRUE(inv)) {
         xm <- t(t(xm) + translation)
     }
     
+    # linear transforms
+    aff_m <- m[seq(2), seq(2)]
+    if (isTRUE(inv)) aff_m <- solve(aff_m)
+    xm <- xm %*% aff_m
+
+    # normal translation
+    if (!is.null(trans) && !isTRUE(inv)) {
+        xm <- t(t(xm) + translation)
+    }
+
     x[, (xcol) := xm[, 1L]]
     x[, (ycol) := xm[, 2L]]
     
