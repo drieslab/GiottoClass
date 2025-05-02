b29ec58c9b4422455a55077a8c2157771d9a30f0
George Chen <72078254+jiajic@users.noreply.github.com>
2025-05-02 10:53:11 -0400

    chore: update globals for spatialdata updates

--- R/globals.R
+++ R/globals.R
@@ -22,6 +22,8 @@ globalVariables(
         "py_tif_convert",
         # spatialdata interop
         "createSpatialData", "read_spatialdata_from_path", "extract_spatial",
-        "extract_image"
+        "extract_image", "extract_spat_enrich", "save_NN_keys", "save_SN_keys",
+        "set_adg_spat_enrich", "save_SE_keys", "extract_polygons",
+        "extract_spat_enrich", "extract_points"
     )
 )
